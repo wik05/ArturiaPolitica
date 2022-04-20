9cb2d51d0ec6d0fea0216f1bcc9e439f158438c9
wik05 <96336866+wik05@users.noreply.github.com>
2022-04-20 18:47:29 +0200

    print list
    output as list

--- dic.py
+++ dic.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import graphviz as graphviz
+
 
 st.title('Proto Dico Encyclopedica Arthuria')
 df = pd.read_csv('dic_struct.csv')
@@ -8,8 +10,13 @@ concept = st.text_input('Enter a concept')
 row = df.loc[df['Concepts'] == concept]
 if concept:
     row = row.dropna(axis=1)
-    for i in row.itertuples():
-        st.write(i)
+    row = row.values.tolist()
+    st.write(row)
+
+
+
+    #for i in row.iteritems():
+    #    st.write(i)
 else:
     st.write('rentrez un concept')
 
