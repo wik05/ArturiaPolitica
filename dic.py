a61841f4eaa5604acd5a45de0ac9130e13b553a4
wik05 <96336866+wik05@users.noreply.github.com>
2022-04-20 17:15:14 +0200

    change title

--- dic.py
+++ dic.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Proto Dicto Science Po')
+st.title('Proto Dico Encyclopedica Arthuria')
 df = pd.read_csv('dic_struct.csv')
 concept = st.text_input('Enter a concept')
 row = df.loc[df['Concepts'] == concept]
